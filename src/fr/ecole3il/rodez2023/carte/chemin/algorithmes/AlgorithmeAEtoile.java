df2a093c91ab65bfa23bd65dfd2d580037366d61
Tom Delalande <tom.delalande@etu.umontpellier.fr>
2024-02-26 16:25:48 +0100

    Implementation AlgorithmeAEtoile

---
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeAEtoile.java
@@ -0,0 +1,58 @@
+package fr.ecole3il.rodez2023.carte.chemin.algorithmes;
+
+import fr.ecole3il.rodez2023.carte.chemin.elements.Graphe;
+import fr.ecole3il.rodez2023.carte.chemin.elements.Noeud;
+
+import java.util.*;
+
+public class AlgorithmeAEtoile<E> implements AlgorithmeChemin<E> {
+    /**
+     * Trouve le chemin le plus court entre deux noeuds dans un graphe en utilisant l'algorithme A*
+     * @param graphe le graphe dans lequel chercher le chemin
+     * @param depart le noeud de départ
+     * @param arrivee le noeud d'arrivée
+     * @return
+     */
+    @Override
+    public List<Noeud<E>> trouverChemin(Graphe<E> graphe, Noeud<E> depart, Noeud<E> arrivee) {
+        Map<Noeud<E>, Double> coutEstime = new HashMap<>();
+        Map<Noeud<E>, Double> coutActuel = new HashMap<>();
+        Map<Noeud<E>, Noeud<E>> predecesseurs = new HashMap<>();
+
+        for (Noeud<E> noeud : graphe.getNoeuds()) {
+            coutEstime.put(noeud, Double.POSITIVE_INFINITY);
+            coutActuel.put(noeud, Double.POSITIVE_INFINITY);
+            predecesseurs.put(noeud, null);
+        }
+        coutEstime.put(depart, 0.0);
+        coutActuel.put(depart, 0.0);
+
+        PriorityQueue<Noeud<E>> filePriorite = new PriorityQueue<>((n1, n2) -> (int) (coutEstime.get(n1) - coutEstime.get(n2)));
+        filePriorite.add(depart);
+
+        while (!filePriorite.isEmpty()) {
+            Noeud<E> noeud = filePriorite.poll();
+            if (noeud.equals(arrivee)) break;
+
+            for (Noeud<E> voisin : graphe.getVoisins(noeud)) {
+                double cout = coutActuel.get(noeud) + graphe.getCoutArete(noeud, voisin);
+                if (cout < coutActuel.get(voisin)) {
+                    predecesseurs.put(voisin, noeud);
+                    coutActuel.put(voisin, cout);
+                    coutEstime.put(voisin, cout);
+                    filePriorite.add(voisin);
+                }
+            }
+        }
+
+        List<Noeud<E>> chemin = new ArrayList<>();
+        Noeud<E> noeud = arrivee;
+        while (noeud != null) {
+            chemin.add(noeud);
+            noeud = predecesseurs.get(noeud);
+        }
+        Collections.reverse(chemin);
+
+        return chemin;
+    }
+}
